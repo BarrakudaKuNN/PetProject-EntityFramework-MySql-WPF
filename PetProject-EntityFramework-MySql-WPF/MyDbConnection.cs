b02b8aa7600cd51531d9e05101f556bb0b386117
Vladislav <Risovanienasaite@gmail.com>
2023-09-06 17:30:12 +0300

    Add project files.

---
+++ PetProject-EntityFramework-MySql-WPF/MyDbConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject_EntityFramework_MySql_WPF
+{
+    internal class MyDbConnection : DbContext
+    {
+        public MyDbConnection():base("")
+        {
+
+        }
+    }
+}
