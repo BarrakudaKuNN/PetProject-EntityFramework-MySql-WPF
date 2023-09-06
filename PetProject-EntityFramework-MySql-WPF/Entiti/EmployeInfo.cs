c3d8a76a56828b5806cae4d87597512fbda96f31
Vladislav <Risovanienasaite@gmail.com>
2023-09-06 18:27:52 +0300

    Created Virtual methods

---
+++ PetProject-EntityFramework-MySql-WPF/Entiti/EmployeInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject_EntityFramework_MySql_WPF.Entiti
+{
+    internal class EmployeInfo
+    {
+    }
+}
